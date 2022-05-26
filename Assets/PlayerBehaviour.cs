0aa51a2612d90e391e0b7b836a13e452d4d9e9f2
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-05-26 09:08:57 -0300

    Did a "chicken"

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject spawnPoint;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon;
+            clon = Instantiate(prefab);
+            clon.transform.position = spawnPoint.transform.position - spawnPoint.transform.forward;
+        }
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed;
+    public float rotationSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+    }
+}

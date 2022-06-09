1ce0ce40a8a67d0b79aa24fba01ecec6c4432711
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-06-09 13:23:32 -0300

    Added bom bom bakudan

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-    public GameObject bomba;
+    public GameObject bala;
     public GameObject spawnPoint;
     public Transform apuntadorTR;
-    Rigidbody prefab;
+    Rigidbody rbBala;
 
     public float fuerzaDisparo;
 
     // Use this for initialization
-    void Start () {
-        prefab = prefab.GetComponent<Rigidbody>();
+    void Start ()
+    {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Q))
         {
-            prefab.AddForce(apuntadorTR.up * fuerzaDisparo, ForceMode.Impulse);
+
+            GameObject clon;
+            clon = Instantiate(bala);
+            rbBala = clon.GetComponent<Rigidbody>();
+            clon.transform.rotation = apuntadorTR.rotation;
+            clon.transform.position = spawnPoint.transform.position - spawnPoint.transform.forward;
+            rbBala.AddForce(clon.transform.up * fuerzaDisparo, ForceMode.Impulse);
+            rbBala.AddForce(apuntadorTR.up * fuerzaDisparo, ForceMode.Impulse);
+            /*
             GameObject clon;
             clon = Instantiate(bomba);
+            clon.transform.position = spawnPoint.transform.position - spawnPoint.transform.forward;
             clon.transform.rotation = apuntadorTR.rotation;
             prefab.AddForce(clon.transform.up * fuerzaDisparo, ForceMode.Impulse);
-            clon.transform.position = spawnPoint.transform.position - spawnPoint.transform.forward;
+            */
         }
 	}
 }

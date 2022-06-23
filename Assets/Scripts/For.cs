59388f4bd6126528187454771036ce30c3951ee1
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-06-23 08:22:37 -0300

    Added for (beware bom bom bakudam)

--- Assets/Scripts/For.cs
+++ Assets/Scripts/For.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class For : MonoBehaviour
 {
     public GameObject prefab;
+    public GameObject spawnPoint;
+    public int num1;
 
 
     public void CloneObject()
     {
-        GameObject clon;
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            for (int i = 0; i < 300; i++)
-            {
-                clon = Instantiate(prefab);
-                clon.transform.Rotate(0, 45 * i, 0);
-                clon.transform.Translate(i * .3f, i, 0);
-
-                //Destroy(clon,.5f);
-            }
+       for (int i = num1; i > 0; i--)
+       {
+            GameObject clon;
+            clon = Instantiate(prefab);
+            clon.transform.position = spawnPoint.transform.position - spawnPoint.transform.forward;
         }
+    }
 
+    private void Start()
+    {
+        CloneObject();
     }
 }
